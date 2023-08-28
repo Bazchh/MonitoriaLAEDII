2135222ef316613965091a2706fcefba4414f95a
Kennedy <kennedy.lopes@ufersa.edu.br>
2023-08-28 10:47:34 -0300

    Funcao para comparação das cartas

--- arvores/arvore.c
+++ arvores/arvore.c
@@ -10,6 +10,10 @@ struct card
     int p4;//0, 1 ou 2
 };
 
+int func(struct card carta){
+    return carta.p1 + 3*carta.p2 + 3*3*carta.p3 + 3*3*3*carta.p4;
+}
+
 struct arvore
 {
     struct card dados;
@@ -37,43 +41,49 @@ int main()
     struct arvore *raiz = (struct arvore *)malloc(sizeof(struct arvore));
     raiz = 0;
 
-    raiz = inserirNovoNo(raiz, 30);
+    struct card carta;
+    carta.p1 = 2;
+    carta.p2 = 1;
+    carta.p3 = 1;
+    carta.p4 = 0;
+
+    raiz = inserirNovoNo(raiz, carta);
     mostrarArvore(raiz);
     printf("\n\n");
-    raiz = inserirNovoNo(raiz, 20);
-    mostrarArvore(raiz);
-    printf("\n\n");
-    raiz = inserirNovoNo(raiz, 12);
-    mostrarArvore(raiz);
-    printf("\n\n");
-    raiz = inserirNovoNo(raiz, 11);
-    mostrarArvore(raiz);
-    printf("\n\n");
-    raiz = inserirNovoNo(raiz, 25);
-    mostrarArvore(raiz);
-    printf("\n\n");
-    raiz = inserirNovoNo(raiz, 27);
-    mostrarArvore(raiz);
-    printf("\n\n");
-    raiz = inserirNovoNo(raiz, 40);
-    mostrarArvore(raiz);
-    printf("\n\n");
-    raiz = inserirNovoNo(raiz, 35);
-    mostrarArvore(raiz);
-    printf("\n\n");
-    raiz = inserirNovoNo(raiz, 41);
-    mostrarArvore(raiz);
-    printf("\n\n");
-    raiz = inserirNovoNo(raiz, 42);
-    mostrarArvore(raiz);
-    printf("\n\n");
-    raiz = inserirNovoNo(raiz, 34);
-    mostrarArvore(raiz);
-    printf("\n\n");
-    raiz = inserirNovoNo(raiz, 36);
-    mostrarArvore(raiz);
-    printf("\n\n");
-    int altura = alturaDaArvore(raiz);
+    // raiz = inserirNovoNo(raiz, 20);
+    // mostrarArvore(raiz);
+    // printf("\n\n");
+    // raiz = inserirNovoNo(raiz, 12);
+    // mostrarArvore(raiz);
+    // printf("\n\n");
+    // raiz = inserirNovoNo(raiz, 11);
+    // mostrarArvore(raiz);
+    // printf("\n\n");
+    // raiz = inserirNovoNo(raiz, 25);
+    // mostrarArvore(raiz);
+    // printf("\n\n");
+    // raiz = inserirNovoNo(raiz, 27);
+    // mostrarArvore(raiz);
+    // printf("\n\n");
+    // raiz = inserirNovoNo(raiz, 40);
+    // mostrarArvore(raiz);
+    // printf("\n\n");
+    // raiz = inserirNovoNo(raiz, 35);
+    // mostrarArvore(raiz);
+    // printf("\n\n");
+    // raiz = inserirNovoNo(raiz, 41);
+    // mostrarArvore(raiz);
+    // printf("\n\n");
+    // raiz = inserirNovoNo(raiz, 42);
+    // mostrarArvore(raiz);
+    // printf("\n\n");
+    // raiz = inserirNovoNo(raiz, 34);
+    // mostrarArvore(raiz);
+    // printf("\n\n");
+    // raiz = inserirNovoNo(raiz, 36);
+    // mostrarArvore(raiz);
+    // printf("\n\n");
+    // int altura = alturaDaArvore(raiz);
     if (verificaBalanceamento(raiz))
     {
         printf("\nA arvore esta balanceada");
@@ -100,13 +110,13 @@ struct arvore *inserirNovoNo(struct arvore *raiz, struct card dados)
 
     // Se a arvore nao estiver vazia, verificamos o valor dentro do nó
     // Se o valor a qual queremos inserir for menor do que o valor do nó atual, inserimos o novo nó a esquerda do nó atual
-    if (dados < raiz->dados)
+    if (func(dados) < func(raiz->dados))
     {
         // É realizado a recursão nos nós esquerdos até que cheguemos no caso base da linha 60
         raiz->esq = inserirNovoNo(raiz->esq, dados);
         // Se o valor a qual queremos inserir for maior do que o valor do nó atual, inserimos o novo nó a direita do nó atual
     }
-    else if (dados > raiz->dados)
+    else if (func(dados) > func(raiz->dados))
     {
         // É realizado a recursão nos nós direitos até que cheguemos no caso base da linha 60
         raiz->dir = inserirNovoNo(raiz->dir, dados);
@@ -121,23 +131,23 @@ struct arvore *inserirNovoNo(struct arvore *raiz, struct card dados)
     raiz->altura = alturaDaArvore(raiz) + 1;
     // Verificando balanceamento do no atual para iniciar processo de balanceamento
     int b = balanceamento(raiz);
-    if (b > 1 && (raiz->esq != 0) && dados < raiz->esq->dados)
+    if (b > 1 && (raiz->esq != 0) && func(dados) < func(raiz->esq->dados))
     {
         return rotacaoDir(raiz);
     }
 
-    if (b < -1 && (raiz->dir != 0) && dados > raiz->dir->dados)
+    if (b < -1 && (raiz->dir != 0) && func(dados) > func(raiz->dir->dados))
     {
         return rotacaoEsq(raiz);
     }
 
-    if (b > 1 && (raiz->esq != 0) && dados > raiz->esq->dados)
+    if (b > 1 && (raiz->esq != 0) && func(dados) > func(raiz->esq->dados))
     {
         raiz->esq = rotacaoEsq(raiz->esq);
         return rotacaoDir(raiz);
     }
 
-    if (b < -1 && (raiz->dir != 0) && dados < raiz->dir->dados)
+    if (b < -1 && (raiz->dir != 0) && func(dados) < func(raiz->dir->dados))
     {
         raiz->dir = rotacaoDir(raiz->dir);
         return rotacaoEsq(raiz);
@@ -234,56 +244,56 @@ struct arvore *rotacaoEsq(struct arvore *raiz)
     return novoNo;
 }
 
-int sucessor(struct arvore *raiz)
-{
-    int s = raiz->dados;
-    while (raiz->esq != 0)
-    {
-        s = raiz->esq->dados;
-        raiz = raiz->esq;
-    }
-    // Removendo o sucessor encontrado para não permitir duplicação de valores
-    raiz->esq = 0;
-    return s;
-}
+// int sucessor(struct arvore *raiz)
+// {
+//     int s = func(raiz->dados);
+//     while (raiz->esq != 0)
+//     {
+//         s = func(raiz->esq->dados);
+//         raiz = raiz->esq;
+//     }
+//     // Removendo o sucessor encontrado para não permitir duplicação de valores
+//     raiz->esq = 0;
+//     return s;
+// }
 
-struct arvore *removerNo(struct arvore *raiz, int dados)
-{
+// struct arvore *removerNo(struct arvore *raiz, int dados)
+// {
 
-    if (raiz == 0)
-    {
-        printf("\nO valor buscado não está na arvore");
-        return 0;
-    }
+//     if (raiz == 0)
+//     {
+//         printf("\nO valor buscado não está na arvore");
+//         return 0;
+//     }
 
-    if (dados < raiz->dados)
-    {
-        raiz->esq = removerNo(raiz->esq, dados);
-    }
-    else if (dados > raiz->dados)
-    {
-        raiz->dir = removerNo(raiz->dir, dados);
-    }
-    else
-    {
+//     if (dados < func(raiz->dados))
+//     {
+//         raiz->esq = removerNo(raiz->esq, dados);
+//     }
+//     else if (dados > func(raiz->dados))
+//     {
+//         raiz->dir = removerNo(raiz->dir, dados);
+//     }
+//     else
+//     {
 
-        if (raiz->esq == 0)
-        {
-            return raiz->dir;
-        }
-        if (raiz->dir == 0)
-        {
-            return raiz->esq;
-        }
+//         if (raiz->esq == 0)
+//         {
+//             return raiz->dir;
+//         }
+//         if (raiz->dir == 0)
+//         {
+//             return raiz->esq;
+//         }
 
-        // Essa função reorganiza a arvore com base no sucessor do nó atual, colocando o sucessor no lugar do nó que queremos remover
-        raiz->dados = sucessor(raiz->dir);
-        // Ao realizar o processo anterior, agora precisamos remover dados duplicados na arvore, neste caso, o sucessor do numero a ser removido que foi encontrado
-        raiz->dir = removerNo(raiz->dir, raiz->dados);
-    }
+//         // Essa função reorganiza a arvore com base no sucessor do nó atual, colocando o sucessor no lugar do nó que queremos remover
+//         raiz->dados = sucessor(raiz->dir);
+//         // Ao realizar o processo anterior, agora precisamos remover dados duplicados na arvore, neste caso, o sucessor do numero a ser removido que foi encontrado
+//         raiz->dir = removerNo(raiz->dir, raiz->dados);
+//     }
 
-    return raiz;
-}
+//     return raiz;
+// }
 
 void mostrarArvore(struct arvore *raiz)
 {
