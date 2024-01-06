7fd16eb40304ffa29b7a077159aa0a66819760e6
Bazch <mikael.vidal@gmail.com>
2024-01-06 10:40:49 -0300

    documentação

--- lista02 - S2/problem2.c
+++ lista02 - S2/problem2.c
@@ -5,7 +5,9 @@
 int main(){
     
     int i;
-    printf("CELSIUS \tFarenheit\n");
+    printf("CELSIUS \tFarenheit\n"); //OBS: /t serve para tabulação
+    //Basicamente nesta questão, devemos utilizar o metodo de conversão de Farenheit para Celsisu para
+    //Cada valor do laço com o i iniciando em 30 e terminando em 50
     for(i = 30; i <= 50; i++){
         printf(" %i   \t  = \t   %i\n",i, (i+32));
     }
